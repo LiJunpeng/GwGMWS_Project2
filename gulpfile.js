861d0e8ec38e659a7158bb7f2f50954ad6ec5d6f
LiJunpeng <lijunpeng91@gmail.com>
2018-08-07 08:13:25 +0800

    add babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
-var gulp = require("gulp");
+const gulp = require("gulp");
+const babel = require("gulp-babel");
 
 gulp.task("default", function () {
 	console.log("23333");
+	return gulp.src("web_src/public/js/*.js")
+		.pipe(babel({
+			presets: ['es2015']
+		}))
+		.pipe(gulp.dest('build'));
 });
