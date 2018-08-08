28dc47152668821769bd692daa8fc6e7ea65a642
LiJunpeng <lijunpeng91@gmail.com>
2018-08-08 08:04:50 +0800

    add gulp workflow

---
+++ build/public/js/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var cacheName = 'restaurant_review_cache';
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(caches.open(cacheName).then(function (cache) {
+    return cache.addAll(['css/index.css', 'css/restaurant.css']);
+  }));
+});
+
+self.addEventListener('fetch', function (event) {
+  console.log("fetch");
+  event.respondWith(caches.match(event.request).then(function (response) {
+    if (response) {
+      return response;
+    } else {
+
+      var fetchRequest = event.request.clone();
+      return fetch(fetchRequest).then(function (response) {
+        var result = response.clone();
+
+        caches.open(cacheName).then(function (cache) {
+          cache.put(event.request, result);
+        });
+
+        return response;
+      });
+    }
+  }));
+});
